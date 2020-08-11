8c4dcd592bdff8081a0cf524de3743c37785c6f2
larysoft <larychi.chukwudi@gmail.com>
2020-08-11 16:39:12 +0100

    Dependency Injection Continuation

---
+++ src/test/java/com/larysoft/diassignment/DiAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larysoft.diassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
